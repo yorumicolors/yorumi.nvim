c81f454ef63e76deb0206722fa197a8bd19347e1
esskayesss <imesskayesss@protonmail.com>
2024-09-22 18:41:12 +0530

    removing minimap. will use a scrollbar instead.

--- lua/yorumi/highlights/init.lua
+++ lua/yorumi/highlights/init.lua
@@ -6,7 +6,7 @@ function M.apply(config)
   local hl_files = {
     "editor", "lsp", "syntax",
     "plugins/nvim-tree", "plugins/nvim-tree",
-    "plugins/indent-blankline", "plugins/code-minimap"
+    "plugins/indent-blankline"
   }
 
   for _, highlight in ipairs(hl_files) do
