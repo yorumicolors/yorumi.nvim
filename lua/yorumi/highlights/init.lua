98f441ca67bdf57a58aa991f92b7e1f9c50530e4
esskayesss <imesskayesss@protonmail.com>
2024-09-23 22:19:17 +0530

    loading treesitter as well

--- lua/yorumi/highlights/init.lua
+++ lua/yorumi/highlights/init.lua
@@ -7,7 +7,7 @@ function M.apply(config)
     "editor", "lsp", "syntax",
     "plugins/nvim-tree", "plugins/nvim-tree",
     "plugins/indent-blankline", "plugins/gitsigns",
-    "plugins/scrollbar", "plugins/cmp"
+    "plugins/scrollbar", "plugins/cmp", "plugins/treesitter"
   }
 
   for _, highlight in ipairs(hl_files) do
